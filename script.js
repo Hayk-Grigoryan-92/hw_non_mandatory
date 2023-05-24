4e82fab29b539552aefd350b7b009ff62b34db37
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2023-05-24 23:41:50 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+// Task 1
+
+// let array = [1,5,7,8,5]
+
+// for(let i = 0; i < array.length; i++){
+//     console.log(array[i]);
+// }
+
+////////////////////////////////////////////
+
+// Task 2 
+
+// function sumNumbers(num1, num2){
+//     let sum = 0
+//     sum = num1 + num2
+//     return sum
+// }
+
+// console.log(sumNumbers(55,14));
+
+/////////////////////////////////////////////
+
+// Task 3
+
+// function sumNumbers(array){
+//     let sum = 0
+//     for(let i=0; i<array.length; i++){
+//         sum += array[i]
+//     }
+//     return sum
+// }
+
+// console.log(sumNumbers([2,4,6]));
+
+//////////////////////////////////////////////
+
+// Task 4
+
+// function summaryArray (array, number){
+//     newArray = array.map(function(value){
+//         return value + number
+//     })
+//     return newArray
+// }
+
+// console.log(summaryArray([4,5,6], 5));
+
+/////////////////////////////////////////////////
+
+// Task 5
+
+// function rectangleSquare(a, b){
+//     return a * b
+// }
+
+// console.log(rectangleSquare(5,4));
+
+////////////////////////////////////////////////
+
+// Task 6 
+
+// function reverseString(data){
+//     let revrese = ''
+//     for(let i=data.length-1; i>=0; i--){
+//         revrese += data[i]
+//     }
+//     return revrese
+// }
+
+// console.log(reverseString('barev'));
+
+//////////////////////////////////////////////////
+
+// Task 7
+
+// function maxNumber (array){
+//     let max = array[0]
+//     for( let i=0; i<array.length; i++){
+//         if(array[i] >= max){
+//             max = array[i]
+//         }
+//     }
+//     return max
+// }
+
+// console.log(maxNumber([120,60,90,500,10]));
+
+//////////////////////////////////////////////////////
